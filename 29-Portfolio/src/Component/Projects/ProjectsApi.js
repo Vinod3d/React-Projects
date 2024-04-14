2ff133e40d7d6a7455f11670e43c63e03124a06f
Vinod Chandra <vinodchandra979@gmail.com>
2024-04-14 10:40:02 +0530

    online product store

--- 29-Portfolio/src/Component/Projects/ProjectsApi.js
+++ 29-Portfolio/src/Component/Projects/ProjectsApi.js
@@ -6,6 +6,7 @@ import Project5 from "../pic/project5.png"
 import Project6 from "../pic/project6.png"
 import Project7 from "../pic/project7.jpg"
 import Project8 from "../pic/project8.jpg"
+import Project9 from "../pic/project9.png"
 
 const ProjectsApi = [
     {
@@ -106,6 +107,19 @@ const ProjectsApi = [
       url : "https://mix-master-rosy.vercel.app/",
       github : 'https://github.com/Vinod3d/Mix-master',
       title_one: "Drinks Api Project",
+      desc_one: "Nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.",
+      title_two: "Nobis eleifend option conguenes.",
+      desc_two: "Mauris tempor, orci id pellentesque convallis, massa mi congue eros, sed posuere massa nunc quis dui. Integer ornare varius mi, in vehicula orci scelerisque sed. Fusce a massa nisi. Curabitur sit amet suscipit nisl. Sed eget nisl laoreet, suscipit enim nec, viverra eros. Nunc imperdiet risus leo, in rutrum erat dignissim id. Ut rhoncus vestibulum facilisis. Duis et lorem vitae ligula cursus venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vitae nisi tortor. Morbi leo nulla, posuere vel lectus a, egestas posuere lacus. Fusce eleifend hendrerit bibendum. Morbi nec efficitur ex.",
+      title_three: "Mauris tempor, orci id pellentesque.",
+      desc_three: "Nulla non ligula vel nisi blandit egestas vel eget leo. Praesent fringilla dapibus dignissim. Pellentesque quis quam enim. Vestibulum ultrices, leo id suscipit efficitur, odio lorem rhoncus dolor, a facilisis neque mi ut ex. Quisque tempor urna a nisi pretium, a pretium massa tristique. Nullam in aliquam diam. Maecenas at nibh gravida, ornare eros non, commodo ligula. Sed efficitur sollicitudin auctor. Quisque nec imperdiet purus, in ornare odio. Quisque odio felis, vestibulum et.",
+    },
+    {
+      id: 9,
+      image: Project9,
+      tech : "html, css, React",
+      url : "https://comfy-ecommerce-store.vercel.app/",
+      github : 'https://github.com/Vinod3d/React-Projects/tree/master/28-Comfy-Store',
+      title_one: "Online Product Store",
       desc_one: "Nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.",
       title_two: "Nobis eleifend option conguenes.",
       desc_two: "Mauris tempor, orci id pellentesque convallis, massa mi congue eros, sed posuere massa nunc quis dui. Integer ornare varius mi, in vehicula orci scelerisque sed. Fusce a massa nisi. Curabitur sit amet suscipit nisl. Sed eget nisl laoreet, suscipit enim nec, viverra eros. Nunc imperdiet risus leo, in rutrum erat dignissim id. Ut rhoncus vestibulum facilisis. Duis et lorem vitae ligula cursus venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vitae nisi tortor. Morbi leo nulla, posuere vel lectus a, egestas posuere lacus. Fusce eleifend hendrerit bibendum. Morbi nec efficitur ex.",
