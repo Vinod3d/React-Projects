61446eca2fa6a8f49addea5dee3336032c26d440
Vinod3d <vinodchandra979@gmail.com>
2026-07-03 21:15:12 +0530

    feat: initialize new React project with Vite, Eslint, and base application structure

---
+++ advance-filtering/src/Products/Products.jsx
@@ -0,0 +1,14 @@
+import Card from "../components/Card";
+import "./Product.css";
+
+const Products = () => {
+  return (
+    <>
+      <section className="card-container">
+        <Card/>
+      </section>
+    </>
+  );
+};
+
+export default Products;
